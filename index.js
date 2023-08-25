165bb61ac55d457464308587d2c0055244e2a887
alexgithubash <alexgithubash>
2023-08-26 00:29:29 +0100

    debug logs deleted

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const { firestore } = require("firebase-admin");
-const { info } = require("firebase-functions/logger");
+
 
 
 const express = require('express')
